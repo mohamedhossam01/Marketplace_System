11a693c80aebf24982b87c1c261ab1575037ae89
ArEl3wam <arelawam@gmail.com>
2022-06-30 23:54:07 +0200

    Creation of Server

--- src/main/java/com/example/marketplace/ServerLogic/Server.java
+++ src/main/java/com/example/marketplace/ServerLogic/Server.java
@@ -1,4 +1,42 @@
 package com.example.marketplace.ServerLogic;
 
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 public class Server {
+    private ServerSocket serverSocket;
+
+    public Server(ServerSocket serverSocket) {
+        this.serverSocket = serverSocket;
+    }
+    public void startServer(){
+        try{
+            while(!serverSocket.isClosed()){
+                Socket socket=serverSocket.accept();
+                ClientHandler clienthandler = new ClientHandler(socket);
+                Thread thread = new Thread(clienthandler);
+                thread.start();
+            }
+        } catch(IOException e){
+
+        }
+    }
+
+    public void closeServerSocket(){
+        try{
+            if(serverSocket != null){
+                serverSocket.close();
+            }
+
+        } catch(IOException e){
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        ServerSocket serverSocket = new ServerSocket(1234);
+        Server server = new Server(serverSocket);
+        server.startServer();
+    }
 }
