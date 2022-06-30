f5c8096bb67b0b95d01132df413ef00bbd9c31c2
ArEl3wam <arelawam@gmail.com>
2022-06-30 23:53:34 +0200

    Creation of ClientHandler

--- src/main/java/com/example/marketplace/ServerLogic/ClientHandler.java
+++ src/main/java/com/example/marketplace/ServerLogic/ClientHandler.java
@@ -1,4 +1,75 @@
 package com.example.marketplace.ServerLogic;
 
-public class ClientHandler {
+import java.io.*;
+import java.net.Socket;
+import java.util.ArrayList;
+
+
+public class ClientHandler implements Runnable{
+
+    public static ArrayList<ClientHandler> clientHandlers = new ArrayList<>();
+    private Socket socket;
+    private BufferedReader bufferedReader;
+    private BufferedWriter bufferedWriter;
+
+    public ClientHandler(Socket socket){
+        try{
+            this.socket=socket;
+            this.bufferedWriter= new BufferedWriter(new OutputStreamWriter( socket.getOutputStream()));
+            this.bufferedReader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            clientHandlers.add(this);
+        }catch(IOException e){
+            closeEverything(socket, bufferedReader,bufferedWriter);
+        }
+    }
+
+    @Override
+    public void run() {
+        String messageFromClient;
+        while(socket.isConnected()){
+            try{
+                messageFromClient = bufferedReader.readLine();
+                broadCastMessage(messageFromClient);
+            }catch(IOException e){
+                closeEverything(socket, bufferedReader , bufferedWriter);
+                break;
+
+            }
+        }
+    }
+    public void broadCastMessage(String messageToSend){
+        for(ClientHandler clientHandler : clientHandlers){
+            try{
+                if(!clientHandler.socket.equals(socket)){
+                    clientHandler.bufferedWriter.write(messageToSend);
+                    clientHandler.bufferedWriter.newLine();
+                    clientHandler.bufferedWriter.flush();
+                }
+            } catch (IOException ex) {
+                closeEverything(socket, bufferedReader,bufferedWriter);
+
+            }
+        }
+    }
+
+    public void removeClientHandler(){
+        clientHandlers.remove(this);
+    }
+    public void closeEverything(Socket socket , BufferedReader  bufferedReader , BufferedWriter bufferedWriter ){
+        removeClientHandler();
+        try{
+            if(bufferedReader !=null){
+                bufferedReader.close();
+            }
+            if(bufferedWriter != null){
+                bufferedWriter.close();
+            }
+            if(socket!=null){
+                socket.close();
+            }
+        } catch(IOException e){
+            e.printStackTrace();
+        }
+
+    }
 }
