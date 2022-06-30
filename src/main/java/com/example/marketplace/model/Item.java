5ff60e983dabb40133ef33708c6dbab5a0b39c72
Ahmed Salah <ahmed_abdelsamie@outlook.com>
2022-06-30 15:23:44 +0200

    modify Item.java

--- src/main/java/com/example/marketplace/model/Item.java
+++ src/main/java/com/example/marketplace/model/Item.java
@@ -1,20 +1,33 @@
 package com.example.marketplace.model;
 
+import java.util.ArrayList;
+
 public class Item {
     private long id;
     private String name;
     private int price;
+    private ArrayList<String> categories;
 
     public Item(){
 
     }
 
-    public Item(long id, String name, int price){
+    public Item(long id, String name, int price, ArrayList<String> categories){
         this.id = id;
         this.name = name;
         this.price = price;
+        this.categories = categories;
     }
 
+	public ArrayList<String> getCategories() {
+		return categories;
+	}
+	public void setCategories(ArrayList<String> categories) {
+		this.categories = categories;
+	}
+	public void setId(long id) {
+		this.id = id;
+	}
 	public Long getId() {
 		return id;
 	}
